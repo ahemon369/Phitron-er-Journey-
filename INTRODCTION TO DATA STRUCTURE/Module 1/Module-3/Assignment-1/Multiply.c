b72a7408a6678db67abd3710b6f399419ef337f2
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-03-21 19:21:33 +0600

    Add files via upload

---
+++ INTRODCTION TO DATA STRUCTURE/Module 1/Module-3/Assignment-1/Multiply.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int A, B;
+
+    scanf("%llf %llf", &A, &B);
+
+    long long int result = A * B;
+
+    printf("%llf \n", result);
+
+    return 0;
+}
