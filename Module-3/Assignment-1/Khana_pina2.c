34349e18b5cd881024c17010fd67e39a11128141
MD AMRAN HOSSIN EMON <mdamranhossinemon78@gmail.com>
2025-03-21 19:20:57 +0600

    Add files via upload

---
+++ Module-3/Assignment-1/Khana_pina2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int M;
+
+    scanf("%d", &M);
+
+    if (M % 3 == 0)
+    {
+        printf("Yes \n");
+    }
+
+    else
+    {
+        printf("No \n");
+    }
+
+    return 0;
+}
